2937509be127eec4c679cc84f21db1dfd975fce7
Giga <gigabyte5671@gmail.com>
2023-05-15 21:54:02 +1200

    Simplify some of the conditional logic

--- src/variablelistener.js
+++ src/variablelistener.js
@@ -11,33 +11,27 @@
 		return value && typeof value === 'object' && !Array.isArray(value);
 	}
 
-	// Compares two stringified objects. Stringification prevents false positives when accidentally comparing prototypes.
-	function jsonCompare (jsonString, object) {
-		return jsonString === JSON.stringify(object);
-	}
-
 	// Compares a variable's current value to its cached value.
 	function variableHasChanged (variable, value) {
-		return isObject(window[value]) ? !jsonCompare(window.variableListenerCache.get(variable)?.value, window[value]) : this.cache[value] !== window[value];
+		return isObject(window[value]) ? window.variableListenerCache.get(variable)?.value !== JSON.stringify(window[value]) : this.cache[value] !== window[value];
 	}
 
-	//The main entry point for VL. Accepts a variable name and a function, adds the variable to the cache and the task to VL.
-	window.addVariableListener = (variableName, callback) => {
-		if (!variableName || !window.hasOwnProperty(variableName)) {
-			console.error("VL | Scope Error: Variable must be globally declared with 'var'.");
-			return false;
-		}
-		if (!callback || typeof callback !== 'function') {
-			console.error("VL | Argument Error: You must pass a function.");
+	// Add a new variable listener. Accepts a variable name and a function to be called whenever the variable changes.
+	window.addVariableListener = (variable, callback) => {
+		if (
+			!variable
+			|| !callback
+			|| typeof callback !== 'function'
+			|| !window.hasOwnProperty(variable)
+		) {
 			return false;
 		}
 
 		// Add the variable and its value to the cache.
-		if (isObject(window[variableName])) {
-			window.variableListenerCache.set(variableName, { value: JSON.stringify(window[variableName]), callback }); // If the value is an object, stringify it to remove inheritance.
-		} else {
-			window.variableListenerCache.set(variableName, { value: window[variableName], callback });
-		}
+		window.variableListenerCache.set(variable, {
+			value: isObject(window[variable]) ? JSON.stringify(window[variable]) : window[variable],
+			callback
+		});
 		return true;
 	};
 
@@ -51,12 +45,9 @@
 		window.variableListenerCache.forEach((variable, data) => {
 			if (variableHasChanged(variable)) {
 				// Update the cached value.
-				if (isObject(window[variable])) {
-					data.value = JSON.stringify(window[variable]) // If the value is an object, stringify it to remove inheritance.
-				} else {
-					data.value = window[variable];
-				}
-				data.callback?.(); // Execute the function.
+				data.value = isObject(window[variable]) ? JSON.stringify(window[variable]) : window[variable];
+				// Execute the function.
+				data.callback?.();
 			}
 		});
 		window.requestAnimationFrame(loop);
