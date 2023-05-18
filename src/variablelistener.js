0a554f99eb28c34e0d684357780af7cda9dc4776
Giga <gigabyte5671@gmail.com>
2023-05-19 10:15:35 +1200

    Add server support through use of `globalThis`

--- src/variablelistener.js
+++ src/variablelistener.js
@@ -1,8 +1,9 @@
 (function () {
-	if (!window) return;
+	const global = window ?? globalThis; // Support client and server.
+	if (!global) return;
 
 	// Stores a copy of all registered variables, allowing the stored value to be compared to the current value.
-	window.variableListenerCache = new Map();
+	global.variableListenerCache = new Map();
 
 	// Normalize object/array values to primative types to remove inheritance.
 	function normalizeValue (value) {
@@ -11,38 +12,38 @@
 
 	// Compares a variable's current value to its cached value.
 	function variableHasChanged (variable, value) {
-		return normalizeValue(value) !== normalizeValue(window[variable]);
+		return normalizeValue(value) !== normalizeValue(global[variable]);
 	}
 
 	// Add a new variable listener. Accepts a variable name and a function to be called whenever the variable changes.
-	window.addVariableListener = (variable, callback) => {
+	global.addVariableListener = (variable, callback) => {
 		if (
 			!variable
 			|| !callback
 			|| typeof callback !== 'function'
-			|| !window.hasOwnProperty(variable)
+			|| !global.hasOwnProperty(variable)
 		) return;
 		// Add the variable and its value to the cache.
-		window.variableListenerCache.set(variable, {
-			value: normalizeValue(window[variable]),
+		global.variableListenerCache.set(variable, {
+			value: normalizeValue(global[variable]),
 			callback
 		});
 	};
 
 	// Removes a listener.
-	window.removeVariableListener = (variable) => window.variableListenerCache.delete(variable);
+	global.removeVariableListener = (variable) => global.variableListenerCache.delete(variable);
 
 	// Starts listening for changes on all registered variables.
 	function loop () {
-		window.variableListenerCache.forEach((variable, data) => {
+		global.variableListenerCache.forEach((variable, data) => {
 			if (variableHasChanged(variable)) {
 				// Update the cached value.
-				data.value = normalizeValue(window[variable]);
+				data.value = normalizeValue(global[variable]);
 				// Execute the function.
 				data.callback?.();
 			}
 		});
-		window.requestAnimationFrame(loop);
+		global.requestAnimationFrame(loop);
 	}
 	loop();
 })();
